809acd8747f4836db04a4b9789342228dca88a4a
Robert Ferney <rob@ferney.org>
2021-04-03 14:03:26 -0600

    fix readme

--- README.rb
+++ README.rb
@@ -7,22 +7,22 @@ find the paths of the line segments that are parallel at a specified distance fr
 ## given the path
 
 
-+ `[(2,2), (4,3), (1,6)]`
++ [(2,2), (4,3), (1,6)]
 
 
 ## The equations for the two lines are 
 
-+ `y = 1/2x + 1`
-+ `y = -1x + 7`
++ y = 1/2x + 1
++ y = -1x + 7
 
 ## the intersection between the lines is at
 
-+ `(4,3)`
++ (4,3)
 
 ## the intersections between the parallel lines are 
 
-+ `(4.197453049082134,4.2167605132909625)`
-+ `(2.3118349659180066,3.273951471708898)`
-+ `(5.688165034081994,2.726048528291102)`
-+ `(3.8025469509178667,1.7832394867090384)`
++ (4.197453049082134,4.2167605132909625)
++ (2.3118349659180066,3.273951471708898)
++ (5.688165034081994,2.726048528291102)
++ (3.8025469509178667,1.7832394867090384)
 
